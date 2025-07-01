810a962953f26ef8620adb91ee79894ab9ad7f4f
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-07-01 17:11:50 -0400

    fix: edited cors configuration in attempt to fix the 405 error

--- server/index.js
+++ server/index.js
@@ -17,6 +17,9 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   }),
 );
 
